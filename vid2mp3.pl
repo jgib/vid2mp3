81a60379e7cf68b65d706b988d92b57db021ac86
jgib <gibersonja@gmail.com>
2013-03-28 12:04:55 -0400

    Not working yet.

--- vid2mp3.pl
+++ vid2mp3.pl
@@ -4,13 +4,14 @@ use warnings;
 
 sub main {
 
-opendir (DIR, .) or die "Failed to open the present working directory.";
+opendir (DIR,".") or die "Failed to open the present working directory.";
 
 while (my $file = readdir(DIR)) {
 
-$file =~ m/(^.+)\.(.+$)/
-system("ffmpeg","-i","$1.$2","-vn","$1.mp3");
-
+$file =~ m/(^.+)\.(.+$)/;
+if ($2 eq "mp4" or "mpeg" or "avi" or "flv") {
+system("ffmpeg","-i $1.$2","-vn $1.mp3");
+}
 }
 
 closedir(DIR);
